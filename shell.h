b14cef598f5e6dc3dc483caa5e12ed996f247e5e
hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-09-02 07:10:55 -0700

    fixing path

--- shell.h
+++ shell.h
@@ -6,8 +6,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+
 void sigint_handler(int signal);
 char **splitline(char *lin);
 char **token_env(char *pat);
+int compile(char **args);
+char *path(char *command);
 
 #endif
